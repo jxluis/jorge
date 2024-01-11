769024d52fec4f64b178ab08ef355419477214f4
jxluis <202100350i@utea.edu.pe>
2024-01-11 09:06:34 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - HTML5</title>
+</head>
+
+<body>
+
+    <header>
+        <h3>Fundamento de programación Web</h3>
+    </header>
+    <section>
+        
+    </section>
+
+    <footer>
+    <h8>Derechos reservados - </h8>
+    </footer>
+</body>
+</html>
